aad6274d0a74c163677024a8e455a117698cdc4f
Prabhat Chaudhary <prabhatc@hyperlinkinfosystem.net.in>
2025-02-05 17:33:27 +0530

    Initial commit

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        alert('Thank you for your message! We will get back to you soon.');
-        form.reset();
-    });
-});
